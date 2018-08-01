8d85a89d63060ebd4783852bff92bb547fc3fc52
patelamisha <41029485+patelamisha@users.noreply.github.com>
2018-08-01 14:59:41 -0400

    MyNewFile

---
+++ server.js
@@ -0,0 +1,23 @@
+var port = 86,
+
+express = require('express'),
+
+app = express().use(express.static(__dirname + '/')),
+
+http = require('http').Server(app);
+
+ 
+
+app.use('/', function(req, res){
+
+    res.sendFile(__dirname + '/checkbox.html');
+
+});
+
+ 
+
+http.listen(port, function(){
+
+    console.log("Node server listening on port " + port);
+
+});
